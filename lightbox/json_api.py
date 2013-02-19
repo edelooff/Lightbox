758b7e02fa1b4080cda3273921a423cc8877ea29
Elmer de Looff <elmer.delooff@gmail.com>
2013-02-19 23:00:16 +0100

    Refactored Lightbox API server
    Added `ControllerInfo` and `StripInfo` methods as well as a very basic delegator method for all GET requests. Filled out the ControllerInfo method, which resolves #3.

--- lightbox/json_api.py
+++ lightbox/json_api.py
@@ -17,49 +17,94 @@ from . import utils
 
 
 class ApiHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  """Ligtbox JSON API Handler."""
   def do_GET(self):
+    """Very basic request router."""
+    if self.path == '/':
+      return self.FormRender()
+    elif self.path == '/info':
+      return self.ControllerInfo()
+    elif self.path == '/strip':
+      return self.StripInfo()
+    return self.ErrorResponse('No path %r. Try the root please.' % self.path)
+
+  def ErrorResponse(self, error):
+    """Something didn't quite go as planned, let's tell the client something."""
+    self.send_response(400)
+    self.send_header('content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(error)
+
+  def ControllerInfo(self):
+    """Returns a JSON object with controller information."""
+    box = self.server.box
+    box_info = {
+        'controller': type(box).__name__,
+        'device': box.port,
+        'baudrate': box.baudrate,
+        'commandrate': box.frequency,
+        'outputs': len(box),
+        'outputrate': float(box.frequency) / len(box)}
+    self.send_response(200)
+    self.send_header('content-type', 'application/json')
+    self.end_headers()
+    self.wfile.write(simplejson.dumps(box_info, sort_keys=True, indent='    '))
+
+  def StripInfo(self):
+    """Returns a JSON object with strip information."""
+    return self.ErrorResponse('StripInfo not implemented')
+
+  def FormRender(self, prefill=''):
+    """Returns a page with a simple HTML form for manual Lightbox controls."""
     self.send_response(200)
     self.send_header('content-type', 'text/html')
     self.end_headers()
     self.wfile.write("""<html><body><form method="post">
-                        <textarea name="json" cols="80" rows="30"></textarea>
-                        <input type="submit" value="push!" />
-                        </form></body></html>""")
+                        <textarea name="json" cols="80" rows="30">%s</textarea>
+                        <br><input type="submit" value="push!" />
+                        </form></body></html>""" % HtmlEscape(prefill))
 
   def do_POST(self):
+    """Processes Lightbox controls via JSON."""
     form = cgi.FieldStorage(
         self.rfile, self.headers, environ={'REQUEST_METHOD': 'POST'})
     try:
       json = simplejson.loads(form.getfirst('json'))
+      if isinstance(json, list):
+        map(self.ProcessCommand, json)
+      else:
+        self.ProcessCommand(json)
+      return self.FormRender(form.getfirst('json'))
     except Exception, error:
-      self.send_response(400)
-      self.end_headers()
-      self.wfile.write(str(error))
-    else:
-      try:
-        if isinstance(json, list):
-          map(self.ProcessCommand, json)
-        else:
-          self.ProcessCommand(json)
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(json)
-      except Exception, error:
-        self.send_response(400)
-        self.end_headers()
-        self.wfile.write(str(error))
+      return self.ErrorResponse(str(error))
 
   def ProcessCommand(self, command):
+    """Performs the given command on the Lightbox instance.
+
+    Envelope and blend method are loaded by name from the utils module. The
+    handling method is selected from a string as well, defaulting to 'Fade' if
+    none is provided.
+    """
     if 'blender' in command:
       command['blender'] = getattr(utils, command['blender'])
     if 'envelope' in command:
       command['envelope'] = getattr(utils, command['envelope'])
-    channel = self.server.controller[command.pop('channel', 0)]
+    channel = self.server.box[command.pop('channel', 0)]
     action = getattr(channel, command.pop('action', 'fade').capitalize())
     action(**command)
 
 
-def ApiServer(controller, port=8000):
+def ApiServer(box, port=8000):
+  """Starts and runs a JSON API server for the given Lightbox controller."""
   server = BaseHTTPServer.HTTPServer(('0.0.0.0', port), ApiHandler)
-  server.controller = controller
+  server.box = box
   server.serve_forever()
+
+
+def HtmlEscape(text):
+  """Prevent XSS problems bij escaping the 5 characters that cause trouble."""
+  text = text.replace('&', '&amp;')
+  text = text.replace('"', '&quot;')
+  text = text.replace("'", '&#39;')  # &apos; is valid, but poorly supported.
+  text = text.replace('>', '&gt;')
+  return text.replace('<', '&lt;')
