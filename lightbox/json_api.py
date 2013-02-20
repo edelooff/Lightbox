d406d85d4eba2dfbac34119e07d12f0b3988e089
Elmer de Looff <elmer.delooff@gmail.com>
2013-02-20 20:47:36 +0100

    Added output information API.
    Added a detailed output info API. This includes the current color of the output (as RGB array and hex string), the output number and number of layers, as well as a detailed specification of each layer. This resolves #4.
    
    For each layer, the report includes the color (as RGB array and hex string), opacity, transition envelope and blend function.
    
    An example of the output (with only the bottom layer set):
    
    ```json
    {
      "mixedColorHex": "#3eb4ce",
      "layers": [
        {
          "colorHex": "#6acfe3",
          "opacity": 1,
          "colorRgb": [106, 207, 227],
          "envelope": "CosineEnvelope",
          "blender": "LabAverage"
        },
        {
          "colorHex": "#000000",
          "opacity": 0,
          "colorRgb": [0, 0, 0],
          "envelope": "CosineEnvelope",
          "blender": "LabAverage"
        },
        {
          "colorHex": "#000000",
          "opacity": 0,
          "colorRgb": [0, 0, 0],
          "envelope": "CosineEnvelope",
          "blender": "LabAverage"
        }
      ],
      "outputNumber": 0,
      "layerCount": 3,
      "mixedColorRgb": [62, 180, 206]
    }
    ```

--- lightbox/json_api.py
+++ lightbox/json_api.py
@@ -20,13 +20,15 @@ class ApiHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   """Ligtbox JSON API Handler."""
   def do_GET(self):
     """Very basic request router."""
-    if self.path == '/':
+    path, _sep, query = self.path.partition('?')
+    params = dict(cgi.parse_qsl(query))
+    if path == '/':
       return self.FormRender()
-    elif self.path == '/info':
+    elif path == '/info':
       return self.ControllerInfo()
-    elif self.path == '/strip':
-      return self.StripInfo()
-    return self.ErrorResponse('No path %r. Try the root please.' % self.path)
+    elif path == '/output':
+      return self.OutputInfo(params)
+    return self.ErrorResponse('No path %r. Try the root please.' % path)
 
   def ErrorResponse(self, error):
     """Something didn't quite go as planned, let's tell the client something."""
@@ -35,34 +37,54 @@ class ApiHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     self.end_headers()
     self.wfile.write(error)
 
+  def HtmlResponse(self, data):
+    """Successful request, send a response to the client as HTML."""
+    self.send_response(200)
+    self.send_header('content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write(data)
+
+  def JsonResponse(self, data):
+    """Successful request, send response to client as JSON."""
+    self.send_response(200)
+    self.send_header('content-type', 'application/json')
+    self.end_headers()
+    self.wfile.write(simplejson.dumps(data))
+
   def ControllerInfo(self):
     """Returns a JSON object with controller information."""
     box = self.server.box
-    box_info = {
+    self.JsonResponse({
         'controller': type(box).__name__,
         'device': box.serial.port,
         'baudrate': box.serial.baudrate,
         'commandrate': box.frequency,
         'outputs': len(box),
-        'outputrate': float(box.frequency) / len(box)}
-    self.send_response(200)
-    self.send_header('content-type', 'application/json')
-    self.end_headers()
-    self.wfile.write(simplejson.dumps(box_info, sort_keys=True, indent='    '))
+        'outputrate': float(box.frequency) / len(box)})
 
-  def StripInfo(self):
-    """Returns a JSON object with strip information."""
-    return self.ErrorResponse('StripInfo not implemented')
+  def OutputInfo(self, params):
+    """Returns a JSON object with Lightbox output information."""
+    if 'id' not in params:
+      return self.ErrorResponse('An argument "id" should be provided')
+    if not params['id'].isdigit():
+      return self.ErrorResponse('Output id should be a number.')
+    output_id = int(params['id'])
+    if not 0 <= output_id < len(self.server.box):
+      return self.ErrorResponse('Output id out of bounds')
+    output = self.server.box[output_id]
+    self.JsonResponse({
+        'outputNumber': output.output_id,
+        'mixedColorRgb': output.color,
+        'mixedColorHex': '#%02x%02x%02x' % tuple(output.color),
+        'layerCount': len(output.layers),
+        'layers': list(LayerReport(output))})
 
   def FormRender(self, prefill=''):
     """Returns a page with a simple HTML form for manual Lightbox controls."""
-    self.send_response(200)
-    self.send_header('content-type', 'text/html')
-    self.end_headers()
-    self.wfile.write("""<html><body><form method="post">
-                        <textarea name="json" cols="80" rows="30">%s</textarea>
-                        <br><input type="submit" value="push!" />
-                        </form></body></html>""" % HtmlEscape(prefill))
+    self.HtmlResponse("""<html><body><form method="post">
+                      <textarea name="json" cols="80" rows="30">%s</textarea>
+                      <br><input type="submit" value="push!" />
+                      </form></body></html>""" % HtmlEscape(prefill))
 
   def do_POST(self):
     """Processes Lightbox controls via JSON."""
@@ -108,3 +130,13 @@ def HtmlEscape(text):
   text = text.replace("'", '&#39;')  # &apos; is valid, but poorly supported.
   text = text.replace('>', '&gt;')
   return text.replace('<', '&lt;')
+
+
+def LayerReport(output):
+  """Yields a dictionary with the state of each layer in the given output."""
+  for layer in output:
+    yield {'blender': layer.blender.__name__,
+           'envelope': layer.envelope.__name__,
+           'colorRgb': layer.color,
+           'colorHex': '#%02x%02x%02x' % layer.color,
+           'opacity': layer.opacity}
