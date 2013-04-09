977ad3e0e8310acaebb4d876bd5d97a4f22448f7
Elmer de Looff <elmer.delooff@gmail.com>
2013-04-09 21:58:04 +0200

    Rewrote JavaScript to consist of separate classes for Lightbox, Output and Layer.
    This allows for easier to follow flow of information and easier attachment of events to update colors and the like.

--- lightbox/static/lightbox.js
+++ lightbox/static/lightbox.js
@@ -1,60 +1,97 @@
 /*jslint indent: 2, plusplus: true */
-"use strict";
 
-$(function() {
-  $.getJSON('/api', createLightbox);
-});
+// Wrap all of this in a self-executing anonymous function. This prevents any
+// scope bleeding, but also means that our "use strict" doesn't affect code
+// outside this source file.
+(function() {
+  "use strict";
 
-function createLightbox(controller) {
-  var index = 0,
-      previewNode = $('#preview'),
-      outputStruct = $('.output').detach();
-
-  // Create a number ouf live view outputs corresponding to the hardware
-  for (index = 0; index < controller.outputCount; index++) {
-    createOutput(outputStruct, previewNode, index);
-  }
-  // Update periodically
-  setInterval(updateLightbox(), 150);
-}
-
-function createOutput(outputStruct, previewNode, outputIndex) {
-  // Adds an output to the previewNode
-  var outputNode = outputStruct.clone();
-  outputNode.find('strong').text('Output ' + outputIndex);
-  previewNode.append(outputNode);
-}
-
-function updateLightbox() {
-  var outputNodes = $('#preview .output');
-  return function() {
-    // Updates the complete live view with current data from the API
-    $.getJSON('/api/outputs', function (outputs) {
-      outputNodes.each(updateOutput(outputs));
+  $(document).ready(function() {
+    $.getJSON('/api', function(data) {
+      document.lightbox = new Lightbox(data);
     });
-  };
-}
+  });
 
-function updateOutput(outputs) {
-  // Updates the mixed color and layer information for a single output
-  return function(outputIndex) {
-    var outputJson = outputs[outputIndex],
-        outputNode = $(this);
-    outputNode.find('.mixed').css(
-        'background-color', outputJson.mixedColorHex);
-    outputNode.find('.layer').each(updateLayerInfo(outputJson));
-  };
-}
+  function Lightbox(controller) {
+    this.node = $('#preview');
+    this.outputTemplate = $('.output').detach();
+    this.outputs = [];
+    this.createOutputs(controller.outputCount);
+    this.update();
+    setInterval(this.update.bind(this), 150);
+  }
 
-function updateLayerInfo(outputJson) {
-  // Updates the color and assorted information for a single output layer
-  return function(listIndex) {
-    var currentLayer = outputJson.layerCount - 1 - listIndex,
-        layerJson = outputJson.layers[currentLayer],
-        layerNode = $(this);
-    layerNode.find('.color').css('background-color', layerJson.colorHex);
-    layerNode.find('.blender').text(layerJson.blender);
-    layerNode.find('.envelope').text(layerJson.envelope);
-    layerNode.find('.opacity').text(Math.round(layerJson.opacity * 100));
+  Lightbox.prototype.createOutputs = function(count) {
+    var index, output;
+    for (index = 0; index < count; index++) {
+      output = new Output(index, this.outputTemplate);
+      this.outputs.push(output);
+      this.node.append(output.node);
+    }
   };
-}
+
+  Lightbox.prototype.update = function () {
+    $.getJSON('/api/outputs', this.updateOutputs.bind(this));
+  };
+
+  Lightbox.prototype.updateOutputs = function(info) {
+    var index;
+    for (index = 0; index < info.length; index++) {
+      this.outputs[index].update(info[index]);
+    }
+  };
+
+  function Output(index, template, layerCount) {
+    this.index = index;
+    this.node = template.clone();
+    this.node.find('strong').text('Output ' + index + 1);
+    this.layerContainer = this.node.find('.layers');
+    this.layerTemplate = this.node.find('.layer').detach();
+    this.layers = [];
+    this.createLayers(layerCount || 3);
+  }
+
+  Output.prototype.createLayers = function(count) {
+    var layer;
+    while (count--) {
+      layer = new Layer(this.layerTemplate, this);
+      this.layers.splice(0, 0, layer);  // Insert at the beginning
+      this.layerContainer.append(layer.node);
+    }
+  };
+
+  Output.prototype.update = function(info) {
+    this.node.find('.mixed').css('background-color', info.mixedColorHex);
+    var i;
+    for (i = 0; i < info.layerCount; i++) {
+      this.layers[i].update(info.layers[i]);
+    }
+  };
+
+  function Layer(template, output) {
+    this.node = template.clone();
+    this.output = output;
+    // Layer color and opacity settings
+    this.color = '#000';
+    this.opacity = 1;
+    this.blender = '';
+    this.envelope = '';
+    // Placed defaults, render this layer
+    this.renderLayer();
+  }
+
+  Layer.prototype.renderLayer = function() {
+    this.node.find('.color').css('background-color', this.color);
+    this.node.find('.opacity').text(Math.round(this.opacity * 100));
+    this.node.find('.blender').text(this.blender);
+    this.node.find('.envelope').text(this.envelope);
+  };
+
+  Layer.prototype.update = function(info) {
+    this.color = info.colorHex;
+    this.opacity = info.opacity;
+    this.blender = info.blender;
+    this.envelope = info.envelope;
+    this.renderLayer();
+  };
+}());
