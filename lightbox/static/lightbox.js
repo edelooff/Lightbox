f702df125e762c105683510014927e0cb16e208e
Elmer de Looff <elmer.delooff@gmail.com>
2013-06-15 15:17:41 +0200

    Spawning colorpicker window on layyer preview clicks.
    Cleaned up the JavaScript and now the colorpicker popup spawns correctly when the layer block is clicked.

--- lightbox/static/lightbox.js
+++ lightbox/static/lightbox.js
@@ -10,21 +10,7 @@
     var lightbox = new Lightbox('#preview');
     lightbox.init();
     lightbox.automaticUpdates(150);
-    window.picker = $('#channel_popup').removeAttr('id').detach();
-    $('#clicker').click(function(event) {
-        $('body').append(window.picker.clone().attr('id', 'channel_popup'));
-        ColorPicker($('#picker')[0], function(hex, hsv, rgb) {
-            $('#tinybox').css('background-color', hex);
-        });
-        $('#channel_popup').lightbox_me({
-            centered: true,
-            onClose: function() {
-                $('#channel_popup').detach();
-                $(".js_lb_overlay").last().remove();
-            }
-        });
-        event.preventDefault();
-    });
+    window.picker = $('#picker_popup').detach();
   });
 
   function Lightbox(node) {
@@ -33,6 +19,7 @@
     this.apiOutputs = '/api/outputs';
     this.outputTemplate = $('.output').detach();
     this.outputs = [];
+    this.info = {};
   }
 
   Lightbox.prototype.automaticUpdates = function(interval) {
@@ -44,11 +31,11 @@
     $.getJSON(this.apiController, this.createOutputs.bind(this));
   };
 
-  Lightbox.prototype.createOutputs = function(controller) {
-    var index, output;
-    for (index = 0; index < controller.outputCount; index++) {
-      output = new Output(index, this.outputTemplate.clone());
-      output.addLayers(controller.layerCount);
+  Lightbox.prototype.createOutputs = function(apiInfo) {
+    var index, output, outputNode;
+    for (index = 0; index < apiInfo.outputCount; index++) {
+      outputNode = this.outputTemplate.clone();
+      output = new Output(index, outputNode, apiInfo);
       this.outputs.push(output);
       this.node.append(output.node);
     }
@@ -65,17 +52,19 @@
     }
   };
 
-  function Output(index, node) {
+  function Output(index, node, apiInfo) {
+    this.apiInfo = apiInfo;
     this.index = index;
     this.node = node;
     this.layerNodes = node.find('.layers');
     this.layerTemplate = node.find('.layer').detach();
     this.layers = [];
     this.setTitle('Output ' + (index + 1));
+    this.addLayers(apiInfo.layerCount);
   }
 
-  Output.prototype.addLayer = function() {
-    var layer = new Layer(this.layerTemplate.clone(), this);
+  Output.prototype.addLayer = function(index) {
+    var layer = new Layer(index, this.layerTemplate.clone(), this);
     this.layers.splice(0, 0, layer);  // Insert at the beginning
     this.layerNodes.append(layer.node);
   };
@@ -83,7 +72,7 @@
   Output.prototype.addLayers = function(count) {
     // Creates a number of layers for the output.
     while (count--) {
-      this.addLayer();
+      this.addLayer(count);
     }
   };
 
@@ -100,8 +89,10 @@
     }
   };
 
-  function Layer(node, output) {
+  function Layer(index, node, output) {
+    this.index = index;
     this.node = node;
+    this.node.click(this.colorPicker.bind(this));
     this.output = output;
     // Default layer color and opacity values
     this.color = '#000';
@@ -111,6 +102,11 @@
     // Placed defaults, render this layer
     this.render();
   }
+
+  Layer.prototype.colorPicker = function(event) {
+    new LayerColorPicker(this);
+    event.preventDefault();
+  };
 
   Layer.prototype.render = function() {
     this.node.find('.color').css('background-color', this.color);
@@ -126,4 +122,55 @@
     this.envelope = layerData.envelope;
     this.render();
   };
+
+  function LayerColorPicker(layer) {
+    this.commandPath = '/api';
+    this.layer = layer;
+    // Color and other variables controlled by the user
+    this.color = '#000';
+    this.opacity = 0;
+    this.immediateUpdate = true;
+    // Initialize color picker
+    this.node = window.picker.clone();
+    this.node.appendTo('body');
+    new ColorPicker($('#picker')[0], this.sliderUpdate.bind(this));
+    this.node.lightbox_me({
+        centered: true,
+        onClose: this.cleanUp.bind(this)
+      });
+  }
+
+  LayerColorPicker.prototype.cleanUp = function() {
+    // Removes the picker and the overlay.
+    this.node.remove();
+    $(".js_lb_overlay").last().remove();
+  };
+
+  LayerColorPicker.prototype.sliderUpdate = function(hex) {
+    this.color = hex;
+    this.node.find('.preview').css('background-color', hex);
+    if (this.immediateUpdate) {
+      this.sendCommand({color: hex});
+    }
+  };
+
+  LayerColorPicker.prototype.setColor = function() {
+    var command = {
+        color: this.color,
+        opacity: this.opacity,
+        steps: this.steps
+      };
+    this.sendCommand(command);
+  };
+
+  LayerColorPicker.prototype.sendCommand = function(command) {
+    command.output = this.layer.output.index;
+    command.layer = this.layer.index;
+    $.ajax(this.commandPath, {
+        data: JSON.stringify(command),
+        contentType: 'application/json',
+        type: 'POST'
+      });
+  };
+
 }());
